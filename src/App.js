1a2871f1273bc8e0f9894139f0dcf03d4e2fcc31
Da-innisfree <dainni.k@gmail.com>
2021-06-22 15:31:53 +0900

    제발

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
-import AppRouter from './component/route/RouterComponent';
+import {BrowserRouter as Router} from "react-router-dom";
+import AppRouter from './RouterComponent';
 import Banner from "./Maincomponent/Banner";
 import Footer from "./Maincomponent/Footer";
+//import {ScrollToTop} from './ScrollToTop';
 
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Banner/>
-        <AppRouter/>
+      <Router >
+        <Banner />
+        {/*<ScrollToTop>*/}
+        <AppRouter />
+        {/*</ScrollToTop>*/}
         <Footer/>
       </Router>
     </div>
